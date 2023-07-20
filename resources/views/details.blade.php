29785fe4c915c587feffbe1eec310e6ea276f6e9
hoatruonghong <70649552+hoatruonghong@users.noreply.github.com>
2023-07-20 17:17:14 +0700

    route param

---
+++ resources/views/details.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="title">
+        <h1>Delicious pizzas - {{ $id }}</h1>
+        </div>
+            
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,12 @@ Route::get('/pizzas', function () {
     ];
     return view('pizzas', [
         'pizzas'=> $pizzas,
-        'name' => request('name'),
-        'age' =>  request('age')
+    ] );
+});
+
+Route::get('/pizzas/{id}', function ($id) {
+    // use the $id to query the db for a record
+    return view('details', [
+        'id' => $id
     ] );
 });
