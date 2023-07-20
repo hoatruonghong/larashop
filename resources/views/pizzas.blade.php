7218eb95ee72c90dea4f1b84dabbafca52d4b7c1
hoatruonghong <70649552+hoatruonghong@users.noreply.github.com>
2023-07-20 17:04:12 +0700

    update css

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -4,7 +4,9 @@
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="title">
         <h1>Delicious pizzas</h1>
+        </div>
             <p>we have:</p>
             @foreach($pizzas as $pizza)
             <div>
